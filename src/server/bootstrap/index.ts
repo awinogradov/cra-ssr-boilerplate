e631e4caab22aee3dad220683898b8f6e90f5154
awinogradov <winogradovaa@gmail.com>
2018-09-17 19:00:46 +0300

    chore(gzip): use compression in prod

--- src/server/bootstrap/index.ts
+++ src/server/bootstrap/index.ts
@@ -1,9 +1,11 @@
 import * as express from 'express';
+import * as compression from 'compression';
 import * as config from 'config';
 
 import { render } from '../render';
 import { run } from '../run';
 
+render.use(compression());
 render.use(express.static(config.get('static.assets')));
 
 (async () => await run(render))();
