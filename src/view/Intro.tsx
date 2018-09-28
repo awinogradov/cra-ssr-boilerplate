26c7794f5670766808bde6b210465ef126ee67ad
awinogradov <winogradovaa@gmail.com>
2018-09-28 21:47:28 +0300

    chore: add react-loadable

---
+++ src/view/Intro.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+export const Intro = () => (
+    <p className="App-intro">
+        To get started, edit <code>src/App.tsx</code> and save to reload.
+    </p>
+);
