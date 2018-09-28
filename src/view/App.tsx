bf1e4c269c7574c99da14cda1157ab50ae9e0621
awinogradov <winogradovaa@gmail.com>
2018-09-28 21:47:28 +0300

    chore: add react-loadable

--- src/view/App.tsx
+++ src/view/App.tsx
@@ -1,6 +1,20 @@
 import * as React from 'react';
+import * as Loadable from 'react-loadable';
 
 import './App.css';
+
+const LoadableIntro = Loadable.Map({
+  loader: {
+    chunk: () => import('./Intro')
+  },
+  loading() {
+    return <div>Loading...</div>
+  },
+  render({ chunk }) {
+    const Intro = chunk.Intro;
+    return <Intro />;
+  }
+});
 
 export class App extends React.Component {
   render() {
@@ -10,9 +24,7 @@ export class App extends React.Component {
           <span className="App-logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <LoadableIntro />
       </div>
     );
   }
